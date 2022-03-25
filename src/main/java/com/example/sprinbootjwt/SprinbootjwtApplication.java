b5ff76204da7340f9da11799be813e0dfb3c2f1b
yoonms0617 <yoonminwater@gmail.com>
2022-03-25 21:51:40 +0900

    Initial commit

---
+++ src/main/java/com/example/sprinbootjwt/SprinbootjwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbootjwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprinbootjwt/SprinbootjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
